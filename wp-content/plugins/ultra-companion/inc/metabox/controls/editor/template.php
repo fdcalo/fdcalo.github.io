5ccc0a06f96623df495012207d0ed51f52a8a1c5
IJBorda <37444801+IJBorda@users.noreply.github.com>
2022-02-20 13:21:27 +0800

    Update design

---
+++ wp-content/plugins/ultra-companion/inc/metabox/controls/editor/template.php
@@ -0,0 +1,13 @@
+<div class="ultra-mb-desc">
+	
+		<span class="butterbean-label">{{ data.label }}</span>
+	
+
+	
+		<span class="butterbean-description">{{{ data.description }}}</span>
+	
+</div>
+
+<div class="ultra-mb-field">
+	<textarea data.attr>{{{ data.value }}}</textarea>
+</div>
---
+++ wp-content/plugins/ultra-companion/inc/metabox/controls/number/template.php
@@ -0,0 +1,13 @@
+<div class="ultra-mb-desc">
+	
+		<span class="butterbean-label">{{ data.label }}</span>
+	
+
+	
+		<span class="butterbean-description">{{{ data.description }}}</span>
+	
+</div>
+
+<div class="ultra-mb-field">
+	<input type="number" value="{{ data.value }}" data.attr>
+</div>
